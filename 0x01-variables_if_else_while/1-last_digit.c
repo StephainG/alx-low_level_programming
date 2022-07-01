c3865fe1a4bab4b382c25853b0237539bebccf77
StephainG <onibukunoluwa@yahoo.com>
2022-07-01 13:50:17 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,6 @@
 /* more headers goes there */
 #include <stdio.h>
 
-
 /* betty style doc for function main goes there */
 /**
  * main - Prints the last digit of a randomly generated number
@@ -17,6 +16,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
