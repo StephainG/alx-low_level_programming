db248953670c30ff37cce84018b7d6563b25887d
StephainG <onibukunoluwa@yahoo.com>
2022-07-05 17:02:30 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,19 +13,19 @@ int main(void)
 	j = 1;
 
 	k = 2;
-
+	
 	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
 		{
-			if (j != 20365011074)
-			{
-				printf("%ld, ", j);
-			} else
-			{
-				printf("%ld\n", j);
-			}
-			next = j + k;
-			j = k;
-			k = next;
+			printf("%ld, ", j);
+		} else
+		{
+			printf("%ld\n", j);
 		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
 	return (0);
 }
