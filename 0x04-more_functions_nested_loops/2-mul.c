757e3bab280b1cdf4a5f78fd2dd1261b9e0d321f
StephainG <onibukunoluwa@yahoo.com>
2022-07-07 06:41:55 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers
+ * @a: first input
+ * @b: seconf input
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int num = mul(34, 56);
+	printf("%d \n", num);
+}
