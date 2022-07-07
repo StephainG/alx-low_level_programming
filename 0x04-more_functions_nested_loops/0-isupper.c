5b679ff1def17f9cec984fa3133b5f4a17f3d4d8
StephainG <onibukunoluwa@yahoo.com>
2022-07-07 06:04:18 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ *
+ * main - check for uppercase letter.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	char c;
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
