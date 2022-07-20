867a078cef140acf61dfd8b9741ae2b5da08f7ac
StephainG <onibukunoluwa@yahoo.com>
2022-07-20 02:47:51 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,12 +4,11 @@
  * _print_rev_recursion - prints a string in reverse
  * @s: printed string
  */
-
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(++s);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
